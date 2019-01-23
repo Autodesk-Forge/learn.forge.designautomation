f27a1348ce6434578b6b4e012e2ba34c11cbdb9a
Augusto Goncalves <augusto.goncalves@autodesk.com>
2019-01-23 10:43:01 -0200

    log trace (todo)

--- updateRVTParam/Commands.cs
+++ updateRVTParam/Commands.cs
@@ -1,7 +1,6 @@
 ﻿using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
-using Autodesk.Revit.DB.Events;
 using DesignAutomationFramework;
 using Newtonsoft.Json;
 using System.Collections.Generic;
@@ -9,75 +8,85 @@ using System.IO;
 
 namespace Autodesk.Forge.Sample.DesignAutomation.Revit
 {
-  [Transaction(TransactionMode.Manual)]
-  [Regeneration(RegenerationOption.Manual)]
-  public class Commands : IExternalDBApplication
-  {
-    //Path of the project(i.e)project where your Window family files are present
-    string OUTPUT_FILE = "OutputFile.rvt";
-
-    public ExternalDBApplicationResult OnStartup(ControlledApplication application)
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Commands : IExternalDBApplication
     {
-      DesignAutomationBridge.DesignAutomationReadyEvent += HandleDesignAutomationReadyEvent;
-      return ExternalDBApplicationResult.Succeeded;
-    }
+        //Path of the project(i.e)project where your Window family files are present
+        string OUTPUT_FILE = "OutputFile.rvt";
 
-    private void HandleDesignAutomationReadyEvent(object sender, DesignAutomationReadyEventArgs e)
-    {
-      e.Succeeded = true;
-      EditWindowParametersMethod(e.DesignAutomationData.RevitDoc);
-    }
-
-    private void EditWindowParametersMethod(Document doc)
-    {
-      InputParams inputParameters = JsonConvert.DeserializeObject<InputParams>(File.ReadAllText("params.json"));
-
-      //Modifying the window parameters
-      //Open transaction
-      using (Transaction trans = new Transaction(doc))
-      {
-        trans.Start("Update window parameters");
-
-        //Filter for windows
-        FilteredElementCollector WindowCollector = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Windows).WhereElementIsNotElementType();
-        IList<ElementId> windowIds = WindowCollector.ToElementIds() as IList<ElementId>;
-
-        foreach (ElementId windowId in windowIds)
+        public ExternalDBApplicationResult OnStartup(ControlledApplication application)
         {
-          Element Window = doc.GetElement(windowId);
-          FamilyInstance FamInst = Window as FamilyInstance;
-          FamilySymbol FamSym = FamInst.Symbol;
-          SetElementParameter(FamSym, BuiltInParameter.WINDOW_HEIGHT, inputParameters.Height);
-          SetElementParameter(FamSym, BuiltInParameter.WINDOW_WIDTH, inputParameters.Width);
+            DesignAutomationBridge.DesignAutomationReadyEvent += HandleDesignAutomationReadyEvent;
+            return ExternalDBApplicationResult.Succeeded;
         }
 
-        //To save all the changes commit the transaction 
-        trans.Commit();
-      }
+        private void HandleDesignAutomationReadyEvent(object sender, DesignAutomationReadyEventArgs e)
+        {
+            LogTrace("Design Automation Ready event triggered...");
+            e.Succeeded = true;
+            EditWindowParametersMethod(e.DesignAutomationData.RevitDoc);
+        }
 
-      //Save the updated file by overwriting the existing file
-      ModelPath ProjectModelPath = ModelPathUtils.ConvertUserVisiblePathToModelPath(OUTPUT_FILE);
-      SaveAsOptions SAO = new SaveAsOptions();
-      SAO.OverwriteExistingFile = true;
+        private void EditWindowParametersMethod(Document doc)
+        {
+            InputParams inputParameters = JsonConvert.DeserializeObject<InputParams>(File.ReadAllText("params.json"));
 
-      //Save the project file with updated window's parameters
-      doc.SaveAs(ProjectModelPath, SAO);
+            //Modifying the window parameters
+            //Open transaction
+            using (Transaction trans = new Transaction(doc))
+            {
+                trans.Start("Update window parameters");
+
+                //Filter for windows
+                FilteredElementCollector WindowCollector = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Windows).WhereElementIsNotElementType();
+                IList<ElementId> windowIds = WindowCollector.ToElementIds() as IList<ElementId>;
+
+                foreach (ElementId windowId in windowIds)
+                {
+                    Element Window = doc.GetElement(windowId);
+                    FamilyInstance FamInst = Window as FamilyInstance;
+                    FamilySymbol FamSym = FamInst.Symbol;
+                    SetElementParameter(FamSym, BuiltInParameter.WINDOW_HEIGHT, inputParameters.Height);
+                    SetElementParameter(FamSym, BuiltInParameter.WINDOW_WIDTH, inputParameters.Width);
+                }
+
+                //To save all the changes commit the transaction 
+                trans.Commit();
+            }
+
+            //Save the updated file by overwriting the existing file
+            ModelPath ProjectModelPath = ModelPathUtils.ConvertUserVisiblePathToModelPath(OUTPUT_FILE);
+            SaveAsOptions SAO = new SaveAsOptions();
+            SAO.OverwriteExistingFile = true;
+
+            //Save the project file with updated window's parameters
+            LogTrace("Saving file...");
+            doc.SaveAs(ProjectModelPath, SAO);
+        }
+
+        public ExternalDBApplicationResult OnShutdown(ControlledApplication application)
+        {
+            return ExternalDBApplicationResult.Succeeded;
+        }
+
+        private void SetElementParameter(FamilySymbol FamSym, BuiltInParameter paraMeter, double parameterValue)
+        {
+            FamSym.get_Parameter(paraMeter).Set(parameterValue);
+        }
+
+        public class InputParams
+        {
+            public double Width { get; set; }
+            public double Height { get; set; }
+        }
+
+        /// <summary>
+        /// This will appear on the Design Automation output
+        /// </summary>
+        private static void LogTrace(string format, params object[] args)
+        {
+            // ToDo
+        }
     }
-
-    public ExternalDBApplicationResult OnShutdown(ControlledApplication application)
-    {
-      return ExternalDBApplicationResult.Succeeded;
-    }
-
-    private void SetElementParameter(FamilySymbol FamSym, BuiltInParameter paraMeter, double parameterValue)
-    {
-      FamSym.get_Parameter(paraMeter).Set(parameterValue);
-    }
-
-    public class InputParams
-    {
-      public double Width { get; set; }
-      public double Height { get; set; }
-    }
-  }
 }
